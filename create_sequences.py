ec8f97fbcb5ece0d9b5d63d1b1c0488dd07e3cc8
Pavithra Ezhilarasan <pavithra.ezhil24@gmail.com>
2018-01-01 06:49:54 +0530

    Add files via upload

---
+++ create_sequences.py
@@ -0,0 +1,52 @@
+# create sequences of images, input sequences and output words for an image
+def create_sequences(tokenizer, max_length, descriptions, photos):
+	X1, X2, y = list(), list(), list()
+	# walk through each image identifier
+	for key, desc_list in descriptions.items():
+		# walk through each description for the image
+		for desc in desc_list:
+			# encode the sequence
+			seq = tokenizer.texts_to_sequences([desc])[0]
+			# split one sequence into multiple X,y pairs
+			for i in range(1, len(seq)):
+				# split into input and output pair
+				in_seq, out_seq = seq[:i], seq[i]
+				# pad input sequence
+				in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+				# encode output sequence
+				out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+				# store
+				X1.append(photos[key][0])
+				X2.append(in_seq)
+				y.append(out_seq)
+	return array(X1), array(X2), array(y)
+
+
+# calculate the length of the description with the most words
+def max_length(descriptions):
+	lines = to_lines(descriptions)
+	return max(len(d.split()) for d in lines)
+
+
+# define the captioning model
+def define_model(vocab_size, max_length):
+	# feature extractor model
+	inputs1 = Input(shape=(4096,))
+	fe1 = Dropout(0.5)(inputs1)
+	fe2 = Dense(256, activation='relu')(fe1)
+	# sequence model
+	inputs2 = Input(shape=(max_length,))
+	se1 = Embedding(vocab_size, 256, mask_zero=True)(inputs2)
+	se2 = Dropout(0.5)(se1)
+	se3 = LSTM(256)(se2)
+	# decoder model
+	decoder1 = add([fe2, se3])
+	decoder2 = Dense(256, activation='relu')(decoder1)
+	outputs = Dense(vocab_size, activation='softmax')(decoder2)
+	# tie it together [image, seq] [word]
+	model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+	model.compile(loss='categorical_crossentropy', optimizer='adam')
+	# summarize model
+	print(model.summary())
+	plot_model(model, to_file='model.png', show_shapes=True)
+	return model
